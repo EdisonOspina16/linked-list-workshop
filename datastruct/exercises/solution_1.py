f19c0f67973ac09649e4bbdff681d194769e4bbb
Edison <edison100ospina@gmail.com>
2025-03-04 11:43:11 -0500

    correcciones primer ejercicio.

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -1,16 +1,25 @@
-from datastruct.classes.lists import LinkedList
+from datastruct.classes.lists import LinkedList, Node
 
 
 def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]:
-
-    lista = LinkedList(0)
+    lista = Node(0)
     actual = lista
-    count = 0
+    acarreo = 0
 
-    while l1 or l2 or count:
-        verl1 = l1.data
-        verl2 = l2.data
+    n1, n2 = l1.head, l2.head
 
-        total = verl1 + verl2
-        actual.next = LinkedList(total % 10)
+    while n1 or n2 or acarreo:
+        valor_l1 = n1.data if n1 else 0
+        valor_l2 = n2.data if n2 else 0
+
+        total = valor_l1 + valor_l2 + acarreo
+        acarreo = total // 10
+        actual.next = Node(total % 10)
         actual = actual.next
+
+        if n1: n1 = n1.next
+        if n2: n2 = n2.next
+
+    resultado = LinkedList[int]()
+    resultado.head = lista.next
+    return resultado
