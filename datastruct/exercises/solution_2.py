464116e72d87bf6bbc87ea531c8efdec385958a0
Edison <edison100ospina@gmail.com>
2025-03-04 14:32:58 -0500

    segundo ejercicio

--- datastruct/exercises/solution_2.py
+++ datastruct/exercises/solution_2.py
@@ -1,6 +1,20 @@
-from datastruct.classes.lists import LinkedList
+from datastruct.classes.lists import LinkedList, Node
 
 
 def swap_nodes_in_pairs(linked_list: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    pass
+    lista = Node(0)
+    lista.next = linked_list.head
+    anterior = lista
+
+    while anterior.next and anterior.next.next:
+        primero = anterior.next
+        segundo = primero.next
+
+        # Intercambio de nodos
+        anterior.next, primero.next, segundo.next = segundo, segundo.next, primero
+
+        # Avanzar al siguiente par
+        anterior = primero
+
+    linked_list.head = lista.next
+    return linked_list
