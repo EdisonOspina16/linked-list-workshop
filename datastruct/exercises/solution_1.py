c63fb87eef92f4b06533441126f02baf3381a8cd
Edison <edison100ospina@gmail.com>
2025-03-03 20:01:30 -0500

    primer ejercicio comenzado

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -2,5 +2,15 @@ from datastruct.classes.lists import LinkedList
 
 
 def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    pass
+
+    lista = LinkedList(0)
+    actual = lista
+    count = 0
+
+    while l1 or l2 or count:
+        verl1 = l1.data
+        verl2 = l2.data
+
+        total = verl1 + verl2
+        actual.next = LinkedList(total % 10)
+        actual = actual.next
